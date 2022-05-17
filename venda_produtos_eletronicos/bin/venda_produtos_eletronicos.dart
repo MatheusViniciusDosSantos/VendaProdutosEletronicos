167620afd087d446d85c531daf6575ba13d001a4
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-05-17 19:48:36 -0300

    Iniciando projeto

---
+++ venda_produtos_eletronicos/bin/venda_produtos_eletronicos.dart
@@ -0,0 +1,5 @@
+import 'package:venda_produtos_eletronicos/venda_produtos_eletronicos.dart' as venda_produtos_eletronicos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${venda_produtos_eletronicos.calculate()}!');
+}
---
+++ venda_produtos_eletronicos/test/venda_produtos_eletronicos_test.dart
@@ -0,0 +1,8 @@
+import 'package:venda_produtos_eletronicos/venda_produtos_eletronicos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
